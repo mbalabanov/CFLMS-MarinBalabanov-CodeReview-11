51b609d6da636456eeaa4806c6824a5b316c9445
Marin Balabanov <marincomics@gmail.com>
2020-11-20 23:10:38 +0200

    Corrected cover image

--- index.php
+++ index.php
@@ -79,9 +79,10 @@
 
     <div class="row">
         <div class="col-12">
-            <div class="embed-responsive embed-responsive-16by9">
+            <img src="assets/video-coverimage.jpg" class="img-fluid">
+             <!-- <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-            </div>
+            </div> -->
         </div>
     </div>
 
